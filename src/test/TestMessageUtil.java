e1de3e231bb1bb0f46d5a568f175bcbbb448f626
Strohm Armstrong <strohma@amazon.com>
2020-06-10 10:41:41 -0700

    Initial commit

---
+++ src/test/TestMessageUtil.java
@@ -0,0 +1,25 @@
+package test;
+
+import main.MessageUtil;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestMessageUtil {
+
+  String message = "Robert";
+  MessageUtil messageUtil = new MessageUtil(message);
+
+  @Test
+  public void testPrintMessage() {
+    System.out.println("Inside testPrintMessage()");
+    assertEquals(message, messageUtil.printMessage());
+  }
+
+  @Test
+  public void testSalutationMessage() {
+    System.out.println("Inside testSalutationMessage()");
+    message = "Hi!" + "Robert";
+    assertEquals(message, messageUtil.salutationMessage());
+  }
+}
